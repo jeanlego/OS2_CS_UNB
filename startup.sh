e49ff5f6374d6a9c0e491fa7c5056525d2f225e5
jeanlego <jeanphilippe.legaul@gmail.com>
2018-01-30 21:22:19 -0400

    Create startup.sh

---
+++ startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USER_NAME=$1
+PASSWD=$2
+
+rm -Rf /workspace/logs
+mkdir -p /workspace/logs
+
+touch /workspace/logs/cloud9.log
+touch /workspace/logs/tigerVNC.log
+touch /workspace/logs/noVNC.log
+
+export CC=gcc
+export CXX=g++
+export DISPLAY=:1
+
+mkdir -p /root/.vnc
+echo PASSWD | vncpasswd -f >> /root/.vnc/passwd
+chmod 600 /root/.vnc/passwd " >> /startup.sh
+
+node /cloud9/server.js --listen 0.0.0.0 --port 8080 -w /workspace --collab -a $USER_NAME:$PASSWD > /workspace/logs/cloud9.log &" >> startup.sh
+
+websockify -D --web=/usr/share/novnc/ 6901 localhost:5901 >> /workspace/logs/noVNC.log &
+vncserver -kill :1 || rm -rfv /tmp/.X*-lock /tmp/.X11-unix
+vncserver :1 -depth 24 -geometry 1280x1024 >> /workspace/logs/tigerVNC.log &" >> startup.sh
+
+xset -dpms &
+xset s noblank &
+xset s off &
+/usr/bin/startxfce4 --replace
