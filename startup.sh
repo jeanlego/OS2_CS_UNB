85f2946143af6adca7930236e0ad71b294e7aeb7
jeanlego <jeanphilippe.legaul@gmail.com>
2018-01-30 21:36:35 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-rm -Rf /workspace/logs
-mkdir -p /workspace/logs
+mkdir /logs
 
-touch /workspace/logs/cloud9.log
-touch /workspace/logs/tigerVNC.log
-touch /workspace/logs/noVNC.log
+touch /logs/cloud9.log
+touch /logs/tigerVNC.log
+touch /logs/noVNC.log
 
 export CC=gcc
 export CXX=g++
@@ -12,13 +11,13 @@ export DISPLAY=:1
 
 mkdir -p /root/.vnc
 echo letmein | vncpasswd -f >> /root/.vnc/passwd
-chmod 600 /root/.vnc/passwd " >> /startup.sh
+chmod 600 /root/.vnc/passwd
 
-node /cloud9/server.js --listen 0.0.0.0 --port 8080 -w /workspace --collab -a root:letmein > /workspace/logs/cloud9.log &" >> startup.sh
+node /cloud9/server.js --listen 0.0.0.0 --port 8080 -w /workspace --collab -a root:letmein > /logs/cloud9.log &
 
-websockify -D --web=/usr/share/novnc/ 6901 localhost:5901 >> /workspace/logs/noVNC.log &
+websockify -D --web=/usr/share/novnc/ 6901 localhost:5901 >> /logs/noVNC.log &
 vncserver -kill :1 || rm -rfv /tmp/.X*-lock /tmp/.X11-unix
-vncserver :1 -depth 24 -geometry 1280x1024 >> /workspace/logs/tigerVNC.log &" >> startup.sh
+vncserver :1 -depth 24 -geometry 1280x1024 >> /logs/tigerVNC.log &
 
 xset -dpms &
 xset s noblank &
