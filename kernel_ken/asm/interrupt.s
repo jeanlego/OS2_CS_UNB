dff1bb4363b94d8ac60aa2d781d0beffb1fcb800
jeanllego <jeanphilippe.legaul@gmail.com>
2018-02-16 20:39:57 +0000

    loose byte

--- kernel_ken/asm/interrupt.s
+++ kernel_ken/asm/interrupt.s
@@ -9,7 +9,7 @@
   global isr%1
   isr%1:
     cli                         ; Disable interrupts firstly.
-    push byte 0                 ; Push a dummy error code.
+    push 0                 ; Push a dummy error code.
     push  %1                    ; Push the interrupt number.
     jmp isr_common_stub         ; Go to our common handler code.
 %endmacro
@@ -30,8 +30,8 @@
   global irq%1
   irq%1:
     cli
-    push byte 0
-    push byte %2
+    push 0
+    push %2
     jmp irq_common_stub
 %endmacro
         
